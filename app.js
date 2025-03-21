65a92f3518614929d4a0b58e49e8900872988667
martin <martinrojasa@outlook.com>
2025-03-21 10:17:34 -0400

    .env

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
