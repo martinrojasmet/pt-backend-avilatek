47264eccc19ce5779360747af885b5d983fb05da
martin <martinrojasa@outlook.com>
2025-03-22 19:53:48 -0400

    db diagram

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -5,7 +5,7 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "E-commerce API",
+            title: "E-commerce API - PT Avila Tek",
             version: "1.0.0",
             contact: {
                 name: "Martin Rojas"
